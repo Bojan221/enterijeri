053007d1e4dc0c2eff61dd510a1fa03a306a4529
Bojan221 <bojan.andzic3@gmail.com>
2025-11-05 15:31:01 +0100

    home page

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".home-slider").slick({
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+     prevArrow:$('.prev'),
+      nextArrow:$('.next'),
+
+  });
+});
